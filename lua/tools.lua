e79ea6c8fd1b6601a1fb171c4d74170a72d1cef5
James Martinez <jamesmartinez.96@outlook.com>
2024-03-01 22:49:35 +1030

    add highlight on yank

--- lua/tools.lua
+++ lua/tools.lua
@@ -1,3 +1,13 @@
+-- Yank Highlight
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
 -- Telescope live_grep in git root
 -- Function to find the git root directory based on the current buffer's path
 local function find_git_root()
