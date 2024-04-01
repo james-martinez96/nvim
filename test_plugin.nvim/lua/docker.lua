c0a75035b4490f610af1c6ede718c50e8c71fb3d
James Martinez <jamesmartinez.96@outlook.com>
2024-04-02 01:48:05 +1030

    created a docker plugin with telescope
    creates a :Docker user command with a custom
    telescope picker that lists all docker images
    that you can select to run

--- test_plugin.nvim/lua/docker.lua
+++ test_plugin.nvim/lua/docker.lua
@@ -9,6 +9,70 @@ local log = require("plenary.log"):new()
 log.level = "debug"
 local M = {}
 
+M.list_containers = function (opts)
+  pickers
+    .new(opts, {
+      finder = finders.new_async_job({
+        command_generator = function ()
+          return {"docker", "container", "list", "-a", "--format", "json"}
+        end,
+
+        entry_maker = function(entry)
+          log.debug(entry)
+          local parsed = vim.json.decode(entry)
+          log.debug(parsed)
+          if parsed then
+            return {
+              value = parsed,
+              display = parsed.Names,
+              ordinal = parsed.Names,
+            }
+          end
+        end,
+      }),
+
+      sorter = config.generic_sorter(opts),
+      previewer = previewers.new_buffer_previewer({
+        title = "Docker Container Details",
+        define_preview = function(self, entry)
+          vim.api.nvim_buf_set_lines(
+            self.state.bufnr,
+            0,
+            0,
+            true,
+            vim.tbl_flatten({
+              "# " .. entry.value.ID,
+              "",
+              "```lua",
+              vim.split(vim.inspect(entry.value), "\n"),
+              "```"
+            })
+          )
+          utils.highlighter(self.state.bufnr, "markdown")
+        end,
+      }),
+      attach_mappings = function(prompt_bufnr)
+        actions.select_default:replace(function()
+          local selection = action_state.get_selected_entry()
+          actions.close(prompt_bufnr)
+
+          log.debug("Selected:", selection)
+          local command = {
+            "edit",
+            "term://docker",
+            "start",
+            "-a",
+            selection.value.Names
+          }
+          log.debug("Command:", command)
+          vim.cmd(vim.fn.join(command, " "))
+        end)
+        return true
+      end
+    })
+    :find()
+end
+
 M.show_docker_images = function(opts)
   pickers
     .new(opts, {
@@ -74,5 +138,6 @@ M.show_docker_images = function(opts)
 end
 
 -- M.show_docker_images()
+-- M.list_containers()
 
 return M
--- test_plugin.nvim/plugin/init.lua
+++ test_plugin.nvim/plugin/init.lua
@@ -1,5 +1,9 @@
 local docker = require("docker")
 
 vim.api.nvim_create_user_command("Docker", function()
-  docker.show_docker_images()
+  docker.show_docker_images({})
+end, {})
+
+vim.api.nvim_create_user_command("DockerContainerList", function()
+  docker.list_containers({})
 end, {})
