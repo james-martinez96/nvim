5f4b83a63c3e346c6ab82e8fd1c6805245053c04
James Martinez <jamesmartinez.96@outlook.com>
2024-12-27 00:42:17 +1030

    fix typo

--- lua/utils.lua
+++ lua/utils.lua
@@ -95,10 +95,11 @@ vim.api.nvim_create_autocmd('TermOpen', {
 
 local job_id = 0
 
-vim.keymap.set("n", "<leader>f", function ()
+vim.keymap.set("n", "<leader>F", function ()
   vim.cmd.vnew()
   vim.cmd.term()
   vim.cmd.wincmd("J")
+  vim.api.nvim_input("i")
   vim.api.nvim_win_set_height(0, 15)
 
   job_id = vim.bo.channel
