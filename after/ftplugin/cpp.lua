b15c4fadd4ef3be91ddd00491601407b7435dd9e
James Martinez <jamesmartinez.96@outlook.com>
2025-04-23 00:29:19 +0930

    feat(ftplugin): c/cpp ftplugin
    added c/cpp filetype options for tabs

---
+++ after/ftplugin/cpp.lua
@@ -0,0 +1,6 @@
+vim.opt_local.expandtab = false        -- Use actual tab characters
+vim.opt_local.tabstop = 8              -- A tab is 8 spaces wide visually
+vim.opt_local.softtabstop = 8          -- How many spaces <Tab> feels like
+-- vim.opt_local.shiftwidth = 8           -- Indentation amount
+vim.opt_local.smartindent = true       -- Auto-indent for C-style syntax
+vim.opt_local.cindent = true           -- More C-aware indentation
