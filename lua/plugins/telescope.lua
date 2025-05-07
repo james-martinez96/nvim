089452684a871e888b5dbceccba41b1b198bed15
James Martinez <jamesmartinez.96@outlook.com>
2025-05-07 15:43:30 +0930

    feat(telescope): add keymap for find_files & live_grep
    with hidden and no-ignore options

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,7 +10,7 @@ return {
     --       refer to the README for telescope-fzf-native for more instructions.
     build = "make",
     opts = function()
-      return vim.fn.executable 'make' == 1
+      return vim.fn.executable("make") == 1
     end,
   },
   opts = {
@@ -52,23 +52,30 @@ return {
     vim.keymap.set("n", "<leader><space>", builtin.buffers, { desc = "[ ] Find existing buffers" })
     vim.keymap.set("n", "<leader>?", builtin.oldfiles, { desc = "[?] Find recently opoend file" })
     vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-    vim.keymap.set(
-      "n",
-      "<leader>sb",
-      builtin.current_buffer_fuzzy_find,
-      { desc = "[S]earch [B]uffer" }
-    )
-    vim.keymap.set('n', '<leader>/', function()
-      -- You can pass additional configuration to telescope to change theme, layout, etc.
-      require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-        winblend = 10,
-        previewer = false,
+    vim.keymap.set("n", "<leader>/sf", function()
+      require("telescope.builtin").find_files({
+        find_command = { "rg", "--files", "--no-ignore", "--hidden" },
       })
-    end, { desc = '[/] Fuzzily search in current buffer' })
+    end, { desc = "[S]earch [F]iles (all, hidden, no-ignore)" })
+    vim.keymap.set("n", "<leader>sb", builtin.current_buffer_fuzzy_find, { desc = "[S]earch [B]uffer" })
+    -- vim.keymap.set('n', '<leader>/', function()
+    --   -- You can pass additional configuration to telescope to change theme, layout, etc.
+    --   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    --     winblend = 10,
+    --     previewer = false,
+    --   })
+    -- end, { desc = '[/] Fuzzily search in current buffer' })
     vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
     vim.keymap.set("n", "<leader>st", builtin.tags, { desc = "[S]earch [T]ags" })
     vim.keymap.set("n", "<leader>sd", builtin.grep_string, { desc = "Search Tags" })
     vim.keymap.set("n", "<leader>sp", builtin.live_grep, { desc = "Live Grep" })
+    vim.keymap.set("n", "<leader>/sp", function ()
+      require("telescope.builtin").live_grep({
+        additional_args = function ()
+          return { "--hidden", "--no-ignore" }
+        end
+      })
+    end , { desc = "Live Grep (hidden, no-ignore)"})
     -- vim.keymap.set("n", "<leader>sp", builtin.grep_string)
     -- vim.keymap.set('n', '<leader>so', require('telescope.builtin').tags{ only_current_buffer = true }, )
   end,
