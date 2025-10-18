9e5c1f499c25d4c7d12b57f25ff035bb51739116
James Martinez <jamesmartinez.96@outlook.com>
2025-10-18 20:15:54 +1030

    chore(lsp): standardize API usage and migrate keymaps to direct function refs

--- lua/new-lsp.lua
+++ lua/new-lsp.lua
@@ -1,5 +1,7 @@
--- PATH for these are being set by "williamboman/mason.nvim"
+local lsp = require('vim.lsp')
+local diagnostic = require('vim.diagnostic')
 
+-- Default configs for these are being set by "williamboman/mason.nvim"
 local lsp_servers = {
   -- LSP for duckyscript
   -- ducky = {
@@ -10,7 +12,7 @@ local lsp_servers = {
   -- stylua = {}, -- need to setup
   bashls = {},
   -- jdtls = {},
-  -- gdtoolkit = {}, -- need to setup
+  gdscript = {}, -- need to setup
   clangd = {},
   -- cpptools = {},
   rust_analyzer = {},
@@ -93,7 +95,7 @@ local lsp_servers = {
 }
 
 -- Get capabilities from nvim-cmp
-local capabilities = vim.lsp.protocol.make_client_capabilities()
+local capabilities = lsp.protocol.make_client_capabilities()
 local has_cmp, cmp_lsp = pcall(require, "cmp_nvim_lsp")
 if has_cmp then
   capabilities = cmp_lsp.default_capabilities(capabilities)
@@ -101,10 +103,10 @@ end
 
 for name, config in pairs(lsp_servers) do
   config.capabilities = capabilities
-  vim.lsp.config(name, config)
+  lsp.config(name, config)
   -- vim.print(name, cfg)
 end
-vim.lsp.enable(vim.tbl_keys(lsp_servers))
+lsp.enable(vim.tbl_keys(lsp_servers))
 
 -- LSP keymaps
 -- vim.api.nvim_create_autocmd("LspAttach", {
@@ -115,7 +117,7 @@ vim.lsp.enable(vim.tbl_keys(lsp_servers))
 -- })
 
 -- Configure diagnostics
-vim.diagnostic.config({
+diagnostic.config({
   virtual_text = {
     -- prefix = "●",
     source = "if_many",
@@ -123,10 +125,10 @@ vim.diagnostic.config({
   -- signs = true,
   signs = {
     text = {
-      [vim.diagnostic.severity.ERROR] = '󰅚',
-      [vim.diagnostic.severity.WARN] = '󰀪',
-      [vim.diagnostic.severity.HINT] = '󰌶',
-      [vim.diagnostic.severity.INFO] = '󰋽',
+      [diagnostic.severity.ERROR] = '󰅚',
+      [diagnostic.severity.WARN] = '󰀪',
+      [diagnostic.severity.HINT] = '󰌶',
+      [diagnostic.severity.INFO] = '󰋽',
     },
     linehl = {
       -- [vim.diagnostic.severity.ERROR] = 'DiagnosticSign',
@@ -152,30 +154,33 @@ vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('my.lsp', {}),
   callback = function(args)
     local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+    local opts = { noremap = true, silent = true }
     -- print(client:supports_method('textDocument/implementation'))
-    vim.keymap.set( "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+    vim.keymap.set( "n", "<leader>q", diagnostic.setloclist, vim.tbl_extend("force", opts, { desc = "Send all diagnostics to qfix list" }))
 
     if client:supports_method('textDocument/implementation') then
         -- Create a keymap for vim.lsp.buf.implementation ...
-        local opts = { noremap = true, silent = true }
-        vim.keymap.set( "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-        vim.keymap.set( "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-        vim.keymap.set( "n", "S", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-        vim.keymap.set( "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-        vim.keymap.set( "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-        vim.keymap.set( "n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-        vim.keymap.set( "n", "<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-        vim.keymap.set( "n", "<leader>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-        vim.keymap.set( "n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-        vim.keymap.set( "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-        vim.keymap.set( "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-        vim.keymap.set( "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-        vim.keymap.set( "n", "<leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-        vim.keymap.set( "n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
-        vim.keymap.set( "n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+        vim.keymap.set( "n", "gD", lsp.buf.declaration, vim.tbl_extend("force", opts, { desc = "goto declaration" }))
+        vim.keymap.set( "n", "gd", lsp.buf.definition, vim.tbl_extend("force", opts, { desc = "goto definition" }))
+        vim.keymap.set( "n", "S", lsp.buf.hover, vim.tbl_extend("force", opts, { desc = "hover info" }))
+        vim.keymap.set( "n", "gi", lsp.buf.implementation, vim.tbl_extend("force", opts, { desc = "goto implementation" }))
+        vim.keymap.set( "n", "<C-k>", lsp.buf.signature_help, vim.tbl_extend("force", opts, { desc = "signature info" }))
+        vim.keymap.set( "n", "<leader>wa", lsp.buf.add_workspace_folder, vim.tbl_extend("force", opts, { desc = "add workspace folder" }))
+        vim.keymap.set( "n", "<leader>wr", lsp.buf.remove_workspace_folder, vim.tbl_extend("force", opts, { desc = "remove workspace folder" }))
+        vim.keymap.set( "n", "<leader>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", vim.tbl_extend("force", opts, { desc = "list workspace folders" }))
+        vim.keymap.set( "n", "<leader>D", lsp.buf.type_definition, vim.tbl_extend("force", opts, { desc = "goto type definition" }))
+        vim.keymap.set( "n", "<leader>rn", lsp.buf.rename, vim.tbl_extend("force", opts, { desc = "rename" }))
+        vim.keymap.set( "n", "gr", lsp.buf.references, vim.tbl_extend("force", opts, { desc = "list all references" }))
+        vim.keymap.set( "n", "<leader>ca", lsp.buf.code_action, vim.tbl_extend("force", opts, { desc = "LSP: code action" }))
+        vim.keymap.set( "n", "<leader>e", diagnostic.open_float, vim.tbl_extend("force", opts, { desc = "show diagnostics" }))
+        vim.keymap.set( "n", "[d", diagnostic.goto_prev, vim.tbl_extend("force", opts, { desc = "Move to prev diagnostic" }))
+        vim.keymap.set( "n", "]d", diagnostic.goto_next, vim.tbl_extend("force", opts, { desc = "Move to next diagnostic" }))
         -- vim.keymap.set( "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-        vim.keymap.set( "n", "<leader>so", [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]], opts)
-        vim.cmd([[ command! Format execute 'lua vim.lsp.buf.format()' ]])
+        vim.keymap.set( "n", "<leader>so", require('telescope.builtin').lsp_document_symbols, {desc = "document symbols"})
+        -- vim.cmd([[ command! Format execute 'lua vim.lsp.buf.format()' ]])
+        vim.api.nvim_create_user_command("Format", function()
+          vim.lsp.buf.format()
+        end, {})
     end
 
     -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
