12c046a135a98090d744edc7e0d49002c5e3e979
James Martinez <jamesmartinez.96@outlook.com>
2025-10-21 21:09:31 +1030

    feat(treesitter): update to main branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,21 +3,18 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     lazy = false,
-    branch = "master",
+    branch = "main",
     -- dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
     build = ":TSUpdate",
-    opts = {
-      highlight = { enable = true },
-      ensure_installed = {
+    config = function(_, opts)
+     local parsers = {
         "c", "cpp", "go", "lua", "python", "rust",
         "tsx", "javascript", "typescript", "vimdoc",
         "vim", "bash", "gdscript", "godot_resource", "gdshader", "c_sharp",
-      },
-      -- auto_install = true,
-    },
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
-
+      }
+      local nvim_treesitter = require("nvim-treesitter")
+      nvim_treesitter.setup({})
+      nvim_treesitter.install(parsers)
       -- register local duckyscript parser
       -- local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
       -- parser_config.duckyscript = {
@@ -35,6 +32,17 @@ return {
       --     ds = "duckyscript",
       --   },
       -- })
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = parsers,
+        callback = function()
+          -- syntax highlighting, provided by Neovim
+          vim.treesitter.start()
+          -- folds, provided by Neovim
+          vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+          -- indentation, provided by nvim-treesitter
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
     end,
   },
   -- {
