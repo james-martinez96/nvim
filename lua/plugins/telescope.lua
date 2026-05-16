83659a1d556f088cdf7f96ac03fe8bb72a44c6f1
James Martinez <jamesmartinez.96@outlook.com>
2026-05-16 18:11:31 +0930

    update telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,47 +1,55 @@
--- TODO:
--- rewrite this maybe...
 return {
     "nvim-telescope/telescope.nvim",
     dependencies = {
         "nvim-lua/plenary.nvim",
-        -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-        -- Only load if `make` is available. Make sure you have the system
-        -- requirements installed.
-        "nvim-telescope/telescope-fzf-native.nvim",
-        -- NOTE: If you are having trouble with this installation,
-        --       refer to the README for telescope-fzf-native for more instructions.
-        build = "make",
-        opts = function()
-            return vim.fn.executable("make") == 1
-        end,
+
+        {
+            "nvim-telescope/telescope-fzf-native.nvim",
+            build = "make",
+            cond = function()
+                return vim.fn.executable("make") == 1
+            end,
+        },
+
+        -- Optional
+        -- "nvim-telescope/telescope-ui-select.nvim",
     },
+
     opts = {
         defaults = {
             layout_strategy = "vertical",
-            layout_config = { height = 0.95 },
+            layout_config = {
+                height = 0.95,
+            },
+
             mappings = {
                 i = {
                     ["<C-u>"] = false,
                     ["<C-d>"] = false,
                 },
             },
+
             file_ignore_patterns = {
-                -- "%.png$",
-                -- "%.jpg$",
                 "^node_modules/",
             },
         },
+
         -- extensions = {
-        --   ['ui-select'] = {
-        --     require('telescope.themes').get_dropdown()
-        --   }
-        -- }
+        --     ["ui-select"] = {
+        --         require("telescope.themes").get_dropdown(),
+        --     },
+        -- },
     },
-    config = function()
-        pcall(require("telescope").load_extension, "fzf")
-        pcall(require("telescope").load_extension, "ui-select")
+
+    config = function(_, opts)
+        local telescope = require("telescope")
+
+        telescope.setup(opts)
+
+        pcall(telescope.load_extension, "fzf")
+        pcall(telescope.load_extension, "ui-select")
+
         local builtin = require("telescope.builtin")
-        -- local opts = { noremap = true, silent = true }
 
         local function telescope_live_grep_open_files()
             builtin.live_grep({
@@ -50,47 +58,106 @@ return {
             })
         end
 
-        vim.keymap.set("n", "<leader>s/", telescope_live_grep_open_files, { desc = "[S]earch [/] in Open Files" })
-        vim.keymap.set("n", "<leader><space>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-        vim.keymap.set("n", "<leader>?", builtin.oldfiles, { desc = "[?] Find recently opened file" })
-        -- vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+        vim.keymap.set(
+            "n",
+            "<leader>s/",
+            telescope_live_grep_open_files,
+            { desc = "[S]earch [/] in Open Files" }
+        )
+
+        vim.keymap.set(
+            "n",
+            "<leader><space>",
+            builtin.buffers,
+            { desc = "[ ] Find existing buffers" }
+        )
+
+        vim.keymap.set(
+            "n",
+            "<leader>?",
+            builtin.oldfiles,
+            { desc = "[?] Find recently opened file" }
+        )
+
         vim.keymap.set("n", "<leader>sf", function()
             builtin.find_files({
                 find_command = {
-                    "rg", "--files",
-                    "--glob", "!*.png",
-                    "--glob", "!*.jpeg",
-                    "--glob", "!*.jpg",
-                    "--glob", "!*.gd.uid",
-                    "--glob", "!node_modules/",
+                    "rg",
+                    "--files",
+
+                    "--glob",
+                    "!*.png",
+
+                    "--glob",
+                    "!*.jpeg",
+
+                    "--glob",
+                    "!*.jpg",
+
+                    "--glob",
+                    "!*.gd.uid",
+
+                    "--glob",
+                    "!node_modules/",
                 },
             })
         end, { desc = "[S]earch [F]iles" })
+
         vim.keymap.set("n", "<leader>/sf", function()
-            require("telescope.builtin").find_files({
-                find_command = { "rg", "--files", "--no-ignore", "--hidden" },
+            builtin.find_files({
+                find_command = {
+                    "rg",
+                    "--files",
+                    "--hidden",
+                    "--no-ignore",
+                },
             })
         end, { desc = "[S]earch [F]iles (all, hidden, no-ignore)" })
-        vim.keymap.set("n", "<leader>sb", builtin.current_buffer_fuzzy_find, { desc = "[S]earch [B]uffer" })
-        -- vim.keymap.set('n', '<leader>/', function()
-        --   -- You can pass additional configuration to telescope to change theme, layout, etc.
-        --   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-        --     winblend = 10,
-        --     previewer = false,
-        --   })
-        -- end, { desc = '[/] Fuzzily search in current buffer' })
-        vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-        vim.keymap.set("n", "<leader>st", builtin.tags, { desc = "[S]earch [T]ags" })
-        vim.keymap.set("n", "<leader>sd", builtin.grep_string, { desc = "Search Tags" })
-        vim.keymap.set("n", "<leader>sp", builtin.live_grep, { desc = "Live Grep" })
+
+        vim.keymap.set(
+            "n",
+            "<leader>sb",
+            builtin.current_buffer_fuzzy_find,
+            { desc = "[S]earch [B]uffer" }
+        )
+
+        vim.keymap.set(
+            "n",
+            "<leader>sh",
+            builtin.help_tags,
+            { desc = "[S]earch [H]elp" }
+        )
+
+        vim.keymap.set(
+            "n",
+            "<leader>st",
+            builtin.tags,
+            { desc = "[S]earch [T]ags" }
+        )
+
+        vim.keymap.set(
+            "n",
+            "<leader>sd",
+            builtin.grep_string,
+            { desc = "[S]earch Current Word" }
+        )
+
+        vim.keymap.set(
+            "n",
+            "<leader>sp",
+            builtin.live_grep,
+            { desc = "[S]earch by [P]attern" }
+        )
+
         vim.keymap.set("n", "<leader>/sp", function()
-            require("telescope.builtin").live_grep({
+            builtin.live_grep({
                 additional_args = function()
-                    return { "--hidden", "--no-ignore" }
-                end
+                    return {
+                        "--hidden",
+                        "--no-ignore",
+                    }
+                end,
             })
-        end, { desc = "Live Grep (hidden, no-ignore)" })
-        -- vim.keymap.set("n", "<leader>sp", builtin.grep_string)
-        -- vim.keymap.set('n', '<leader>so', require('telescope.builtin').tags{ only_current_buffer = true }, )
+        end, { desc = "[S]earch [P]attern (hidden, no-ignore)" })
     end,
 }
